0a6bc79d4706cafe62d5e517cf34bfad399dda2f
CarlosAI <carloswesker@outlook.es>
2019-03-20 23:21:11 -0600

    Succes API

---
+++ src/prueba/HttpURLConnectionExample.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class HttpURLConnectionExample {
+
+	private final String USER_AGENT = "Mozilla/5.0";
+        private final int usuario = 147;
+
+	public static void main(String[] args) throws Exception {
+
+		HttpURLConnectionExample http = new HttpURLConnectionExample();
+
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet();
+
+
+	}
+
+	// HTTP GET request
+	public void sendGet() throws Exception {
+
+		String url = "https://www.marketful.mx/consultar_usuario?user_id="+usuario;
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+                con.setRequestProperty("Content-Type", "application/json"); 
+                con.setRequestProperty("Accept", "application/json");
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+	
+	// HTTP POST request
+	
+
+}
